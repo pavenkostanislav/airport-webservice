7fe8c1f02542f948b1a6d5965cc24ade8f2f6511
Stanislav Pavenko <pavenko@i-teco.ru>
2019-06-17 14:08:25 +0300

    wip

--- src/main/java/com/example/demo/dao/IAlien.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Alien;
-
-public interface IAlien extends CrudRepository<Alien, Integer> {
-
-}
