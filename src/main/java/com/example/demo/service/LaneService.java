af5e36d4db62c3e61e3b8ed6e0fe599fc2d506cc
Stanislav Pavenko <pavenko@i-teco.ru>
2019-06-20 15:38:50 +0300

    wip

--- src/main/java/com/example/demo/service/LaneService.java
+++ src/main/java/com/example/demo/service/LaneService.java
@@ -3,19 +3,23 @@ package com.example.demo.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.dao.ILaneRepository;
 import com.example.demo.model.Lane;
 
 @Component
+@EnableCaching
+@CacheConfig(cacheNames = "LaneServiceCache")
 public class LaneService extends GenericService<Lane, ILaneRepository> {
-    private static final Logger logger = LoggerFactory.getLogger(Lane.class);
+    private static final Logger logger = LoggerFactory.getLogger(LaneService.class);
 	@Override
-	@Cacheable(cacheNames="getLane")
+	@Cacheable(sync=true)
 	public Lane get(ILaneRepository crudRepository, Long id) {
-        logger.info("get lane: id=" + id);
+        logger.info("called method get with params: crudRepository: " + crudRepository.toString() + " id=" + id);
 		return super.get(crudRepository, id);
 	}
 }
